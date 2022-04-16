3ab5bbf8c9c2455c13d7f06c72fd300148e14654
Aldy Akbarrizky <AldyAkbarrizky18@gmail.com>
2022-04-16 17:07:55 +0700

    :boom: Update Test Case 1 & Test Case 2

--- src/test/java/NumberDifferenceTest.java
+++ src/test/java/NumberDifferenceTest.java
@@ -7,10 +7,18 @@ import java.util.Arrays;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import NumberDifference.CalcNUmberDifference;
+import NumberDifference.App;
 
 public class NumberDifferenceTest {
 	private CalcNUmberDifference sut;
+	private App main_sut;
 
 	/**
 	 * Setup = inisiasi sebelum test dijalankan, preparation object
@@ -30,33 +38,51 @@ public class NumberDifferenceTest {
 	 */
 
 	/**
-	 * Test Case No. 1
+	 * Test Case No. 1 (Dibuat oleh Afdal dan diupdate oleh Aldy)
 	 */
 	@Test
 	public void testNRangeNoValidLowBoundary() {
-		int jumlahDeretBil;
-		boolean actual, expected;
-		jumlahDeretBil = 0;
-		expected = false;
+		String expectedMessage = "Number difference can not be processed";
+		InputStream stdin = System.in;
+		System.setIn(new ByteArrayInputStream("0".getBytes()));
 
-		actual = sut.validateRangeInputMaxDeret(jumlahDeretBil);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(byteArrayOutputStream);
+		PrintStream stdout = System.out;
+		System.setOut(ps);
 
-		assertEquals(expected, actual);
+		App.main(new String[0]);
+
+		System.setIn(stdin);
+		System.setOut(stdout);
+
+		String outputText = byteArrayOutputStream.toString();
+		String output = outputText.substring(outputText.indexOf(expectedMessage)).trim();
+		assertEquals(expectedMessage, output);
 	}
 
 	/**
-	 * Test Case No. 2
+	 * Test Case No. 2 (Dibuat oleh Afdal dan diupdate oleh Aldy)
 	 */
 	@Test
 	public void testNRangeNoValidUpBoundary() {
-		int jumlahDeretBil;
-		boolean actual, expected;
-		jumlahDeretBil = 11;
-		expected = false;
+		String expectedMessage = "Number difference can not be processed";
+		InputStream stdin = System.in;
+		System.setIn(new ByteArrayInputStream("11".getBytes()));
 
-		actual = sut.validateRangeInputMaxDeret(jumlahDeretBil);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(byteArrayOutputStream);
+		PrintStream stdout = System.out;
+		System.setOut(ps);
 
-		assertEquals(expected, actual);
+		App.main(new String[0]);
+
+		System.setIn(stdin);
+		System.setOut(stdout);
+
+		String outputText = byteArrayOutputStream.toString();
+		String output = outputText.substring(outputText.indexOf(expectedMessage)).trim();
+		assertEquals(expectedMessage, output);
 	}
 
 	/**
